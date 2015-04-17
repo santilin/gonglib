89ecba61c85879ae37b8c6f1cae50176920dcdd6
santilin <santi@programacionsocial.net>
2015-04-17 08:43:20 +0200

    Arreglado capel de factu. Cambiada fecha de construcción

--- mod/factu/factufrmeditagente.cpp
+++ mod/factu/factufrmeditagente.cpp
@@ -35,6 +35,7 @@ FrmEditAgente::FrmEditAgente(FrmEditRec *parentfrm, dbRecord *master, dbRecordDa
 	editRazonSocial = addEditField( pControlsFrame, "AGENTE", "RAZONSOCIAL", codigoLayout );
 	editNotas = addTextField( pControlsFrame, "AGENTE", "NOTAS", notasLayout );
 	pControlsLayout->addLayout( codigoLayout );
+	pControlsLayout->addWidget( pFrameContactos );
 	pControlsLayout->addLayout( notasLayout );
 /*>>>>>FRMEDITAGENTE_INIT_CONTROLS*/
 // {capel} Añadir detrás de addLayout( codigoLayout )
--- mod/factu/factufrmeditproveedora.cpp
+++ mod/factu/factufrmeditproveedora.cpp
@@ -83,6 +83,7 @@ if( ModuleInstance->getContabModule() ) {
 	checkUsaReferencias = addCheckField( frameGenCodArt, "PROVEEDORA", "USAREFERENCIAS", usareferenciasLayout );
 	editNotas = addTextField( pControlsFrame, "PROVEEDORA", "NOTAS", notasLayout );
 	pControlsLayout->addLayout( codigoLayout );
+	pControlsLayout->addWidget( pFrameContactos );
 	pControlsLayout->addLayout( formapagoLayout );
 	pControlsLayout->addLayout( nombrealtLayout );
 	pControlsLayout->addLayout( agenteLayout );
